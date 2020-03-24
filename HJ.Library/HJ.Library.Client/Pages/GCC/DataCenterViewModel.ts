141b9fde19d87931e85354e7e01f09a7f898219f
Frank.Sun <Frank.Sun@LP00>
2020-03-24 21:15:44 +0800

    Map investigation done. 用qq.map. baidu and gaode is not totally free.

--- HJ.Library/HJ.Library.Client/Pages/GCC/DataCenterViewModel.ts
+++ HJ.Library/HJ.Library.Client/Pages/GCC/DataCenterViewModel.ts
@@ -1,8 +1,9 @@
 ﻿///<reference path="../PageBase.ts" />
 //import G2 from '../../Scripts/g2.min.js';
 
-module hj.library.pages {    
+module hj.library.pages {
     declare var Chart;
+    declare var qq;
     export class DataCenterViewModel extends PageBase {
         private isC1Loaded = ko.observable(false);
         private isMainVideoContainerLoaded = ko.observable(false);
@@ -35,48 +36,79 @@ module hj.library.pages {
             // https://wiki.videolan.org/Documentation:WebPlugin/#Playlist_items_object
             // By link above, can use playlist to implement (轮播, 自己写方法切换playitem index), 
             // Can also clear playlist to play another video
-            //this.mainVideoContainer().playlist.add("rtsp://wowzaec2demo.streamlock.net/vod/mp4:BigBuckBunny_115k.mov");
-            //this.mainVideoContainer().playlist.playItem(0);
+            this.mainVideoContainer().playlist.add("rtsp://wowzaec2demo.streamlock.net/vod/mp4:BigBuckBunny_115k.mov");
+            this.mainVideoContainer().playlist.playItem(0);
 
-            setTimeout(() => {
-                var ctx = document.getElementById('myChart');
-                var myChart = new Chart(ctx, {
-                    type: 'bar',
-                    data: {
-                        labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
-                        datasets: [{
-                            label: '# of Votes',
-                            data: [12, 19, 3, 5, 2, 3],
-                            backgroundColor: [
-                                'rgba(255, 99, 132, 0.2)',
-                                'rgba(54, 162, 235, 0.2)',
-                                'rgba(255, 206, 86, 0.2)',
-                                'rgba(75, 192, 192, 0.2)',
-                                'rgba(153, 102, 255, 0.2)',
-                                'rgba(255, 159, 64, 0.2)'
-                            ],
-                            borderColor: [
-                                'rgba(255, 99, 132, 1)',
-                                'rgba(54, 162, 235, 1)',
-                                'rgba(255, 206, 86, 1)',
-                                'rgba(75, 192, 192, 1)',
-                                'rgba(153, 102, 255, 1)',
-                                'rgba(255, 159, 64, 1)'
-                            ],
-                            borderWidth: 1
+            var ctx = document.getElementById('myChart');
+            var myChart = new Chart(ctx, {
+                type: 'bar',
+                data: {
+                    labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
+                    datasets: [{
+                        label: '# of Votes',
+                        data: [12, 19, 3, 5, 2, 3],
+                        backgroundColor: [
+                            'rgba(255, 99, 132, 0.2)',
+                            'rgba(54, 162, 235, 0.2)',
+                            'rgba(255, 206, 86, 0.2)',
+                            'rgba(75, 192, 192, 0.2)',
+                            'rgba(153, 102, 255, 0.2)',
+                            'rgba(255, 159, 64, 0.2)'
+                        ],
+                        borderColor: [
+                            'rgba(255, 99, 132, 1)',
+                            'rgba(54, 162, 235, 1)',
+                            'rgba(255, 206, 86, 1)',
+                            'rgba(75, 192, 192, 1)',
+                            'rgba(153, 102, 255, 1)',
+                            'rgba(255, 159, 64, 1)'
+                        ],
+                        borderWidth: 1
+                    }]
+                },
+                options: {
+                    scales: {
+                        yAxes: [{
+                            ticks: {
+                                beginAtZero: true
+                            }
                         }]
-                    },
-                    options: {
-                        scales: {
-                            yAxes: [{
-                                ticks: {
-                                    beginAtZero: true
-                                }
-                            }]
-                        }
                     }
+                }
+            });
+
+            var map = new qq.maps.Map(
+                document.getElementById("mapContainer"),
+                {
+                    center: new qq.maps.LatLng(34.766670, 113.67000),
+                    zoom: 13
                 });
-            }, 2000);
-        }
+
+            var marker = new qq.maps.Marker({
+                position: new qq.maps.LatLng(34.753803, 113.661693),
+                map: map
+            });
+
+            var info = new qq.maps.InfoWindow({ map: map });
+
+            qq.maps.event.addListener(marker, 'click', () => {
+                info.open();
+                info.setContent(`<embed type="application/x-vlc-plugin" pluginspage="http://www.videolan.org" 
+                                    src= "rtsp://wowzaec2demo.streamlock.net/vod/mp4:BigBuckBunny_115k.mov"
+                                   width= "640"
+                                   height= "480"
+                                   id= "vlcin" />`);
+                info.setPosition(new qq.maps.LatLng(34.753803, 113.661693)); 
+            });
+
+            // info里面的内容并没有真正remove掉，所以添加这个事件来处理
+            qq.maps.event.addListener(info, 'closeclick', function () {                
+                info.setContent('');
+                //info.destroy();//没有destory 方法，webgl版本里面才有
+
+                //即使上面set to empty了，这个元素还在，还是要手动用jquery找到并remove掉
+                $("embed#vlcin").remove();
+            });
+        };
     }
 }
